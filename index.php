d7dbfc85f910d24ea68e1665d3a06a76cfcd255b
Gauravbhatt19 <gauravbhatt1924@gmail.com>
2018-10-01 00:01:43 +0530

    login page final

--- index.php
+++ index.php
@@ -19,24 +19,44 @@
 			<h1>Internal Examination Portal</h1>
 		</nav>
 	</header>
-	<div class="">
-		<form action="" class="bx">
+		<div class="xyz">
+			
+<div class="bd">
+		<div class="bx" id="log">
+	      	<h1>News</h1>
+	      	<ul>
+	      	<li><h3>heading</h3><p>here is content. <a href="#"  class="hglt">Attachment</a></p><h6> Date :30 Sept 18 8:48 PM</h6></li>
+	      		<li><h3>heading1</h3><p>here is content2. <a href="#"  class="hglt">Attachment</a></p><h6> Date :30 Sept 18 8:48 PM<</h6></li>
+	      		<li><h3>heading2</h3><p>here is content3. <a href="#"  class="hglt">Attachment</a></p><h6> Date :30 Sept 18 8:48 PM</h6></li>
+	      		</ul>
+	      	</div>
+	      </div>
+	      		
+	      		
+
+<div class="lg">
+		<form action="./_login.php" class="bx" onsubmit="validate()" method="post">
 			<div>
-				<label for="login-type">Login as : </label>
-				<select id='login-type'>
+				<label for="login-type">Login as </label>			</div>
+				<div class="bo"><select id='login-type'>
 					<option selected>Student</option>
 					<option>Faculty</option>	
-				</select>
+				</select></div>
+
+			<div>
+				<label for="login-id">Id</label></div>
+				<div class="bo"><input type="text" id='login-id' placeholder="Login Id">
 			</div>
 			<div>
-				<label for="login-id">Login Id : </label>
-				<input type="text" id='login-id' placeholder="Login Id">
+				<label for="login-pass">Password</label></div>
+				<div class="bo"><input type="Password" id='login-pass' placeholder="Login Password">
 			</div>
-			<div>
-				<label for="login-pass">Login Password : </label>
-				<input type="Password" id='login-pass' placeholder="Login Password">
+			<div class="bo">
+				<input type="submit" value="Login " >
 			</div>
 		</form>
 	</div>
-</body>
+</div>
+<footer><p>Designed by Spiders, Copyright &copy; Examination Department, 2018 </p></footer><div class="cnt">
+	</body>
 </html>
